b2d9314f2ae510acb47c6a296c8c582ae74aa1ca
Yoko Zoko <kawaiieraser@protonmail.com>
2018-09-30 04:08:39 -0700

    ``

--- Main.py
+++ Main.py
@@ -9,9 +9,12 @@ def request():
     except ConnectionError:
         print("[-] Failed to retrieve from website")
 
-    data = yaml.load(http_response)
+    data = yaml.load(http_response).split(' ')
     print(data)
-    print('[+] Job Complete')
+    #[{station_id: 0, angles_degrees: 90}, {}]
+    print("station_id angle_degrees")
+    for value in data[2:]:
+        print(value)
 
 
 if __name__ == '__main__':
