37265f98f64bc388ae16e58c2806600761bc2f5f
Yoko Zoko <kawaiieraser@protonmail.com>
2018-09-30 17:57:31 -0700

    ``

--- Main.py
+++ Main.py
@@ -10,12 +10,11 @@ def request():
         print("[-] Failed to retrieve from website")
 
     data = http_response.read().decode('utf-8').replace('\r', '').split('\n')[:-1]
-    print(data)
-    print('------------')
+
     print(data[0])
     for info in data[1:]:
-        bracket = info.split(',')
-        print(bracket[0], '\t\t\t', bracket[1], '           ', math.cos(int(bracket[1])))
+        datainfo = info.split(',')
+        print(datainfo[0], '\t\t\t', datainfo[1], '           ', math.cos(int(datainfo[1])))
 
 
 if __name__ == '__main__':
