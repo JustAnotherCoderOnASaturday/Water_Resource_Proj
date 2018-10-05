74d040e21c4f71024755372472023541cd3d9550
Yoko Zoko <kawaiieraser@protonmail.com>
2018-10-05 02:01:21 -0700

    ``

--- Main.py
+++ Main.py
@@ -3,18 +3,14 @@ import math
 
 
 def request():
-    try:
-        url = "http://rapid-hub.org/data/angles_UCI_CS.csv"
-        http_response = urllib.request.urlopen(url)
-    except ConnectionError:
-        print("[-] Failed to retrieve from website")
-
+    url = "http://rapid-hub.org/data/angles_UCI_CS.csv"
+    http_response = urllib.request.urlopen(url)
     data = http_response.read().decode('utf-8').replace('\r', '').split('\n')[:-1]
 
     print(data[0])
     for info in data[1:]:
         datainfo = info.split(',')
-        print(datainfo[0], '\t\t\t', datainfo[1], '           ', math.cos(int(datainfo[1])))
+        print('{data0}\t\t{data1}\t\t{data2}'.format(data0=datainfo[0], data1=datainfo[1], data2=math.cos(int(datainfo[1]))))
 
 
 if __name__ == '__main__':
